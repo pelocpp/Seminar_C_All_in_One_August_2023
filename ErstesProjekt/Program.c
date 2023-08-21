d16bb065b6afa6ece522bfabad1c7a7c30e9a06a
Admin <Admin@MERKUR01-34>
2023-08-21 10:48:19 +0200

    Projektdateien hinzufügen.

---
+++ ErstesProjekt/Program.c
@@ -0,0 +1,29 @@
+#include <stdio.h>  // standard input & output 
+
+// Einsprungpunkt
+void main_hello_world()
+{
+	// Ausgabe auf die Konsole
+	printf("Hello World");
+}
+
+// ==================================
+
+void main_datentyp_und_variable()
+{
+	// Datentyp und Variable
+
+	int zahl = 1;
+
+	int zahlZwei;
+
+	zahlZwei = zahl;
+
+	// zahl = 123;
+}
+
+
+void main()
+{
+	// Kontrollstruktur
+}
