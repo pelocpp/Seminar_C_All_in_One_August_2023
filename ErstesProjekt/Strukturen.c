94f082085ccd0c68bb693aa49ab1fc209f06bbb2
Peter <peter.loos@gmx.de>
2023-08-25 12:10:34 +0200

    Stand Freitag Mittag.

--- ErstesProjekt/Strukturen.c
+++ ErstesProjekt/Strukturen.c
@@ -1,4 +1,5 @@
 #include <stdio.h>  
+#include <assert.h>  
 
 #include "Time.h"
 #include "Vector.h"
@@ -38,8 +39,24 @@ void printTime(const struct Time* t)
 
 void incrementTime(struct Time* t)
 {
-	// very, very simple --- mostly wrong
-	t->seconds = t->seconds + 1;
+	t->seconds++;  // simple
+
+	if (t->seconds >= 60)
+	{
+		t->seconds = 0;
+		t->minutes++;
+
+		if (t->minutes >= 60)
+		{
+			t->minutes = 0;
+			t->hours++;
+
+			if (t->hours >= 24)
+			{
+				t->hours = 0;
+			}
+		}
+	}
 }
 
 void initTime(struct Time* t, int hours, int minutes, int seconds)
@@ -70,7 +87,6 @@ void strukturen_uebung()
 
 	incrementTime(&t);
 	printTime(&t);
-
 }
 
 void strukturen_die_erste()
@@ -96,7 +112,39 @@ void strukturen_die_erste()
 	int aktuelleStunde = jetzt.hours;
 }
 
+// =================================================================
+
+union WieSiehtEinIntegerAus
+{
+	unsigned int anInt;
+
+	unsigned char bytes[ sizeof (unsigned int)];
+};
+
+void testUnion ()
+{
+	assert(sizeof(unsigned int) == 4);
+
+	union WieSiehtEinIntegerAus testInt;
+
+	testInt.anInt = 12345678;
+
+	unsigned char teil1 = testInt.bytes[0];
+	unsigned char teil2 = testInt.bytes[1];
+	unsigned char teil3 = testInt.bytes[2];
+	unsigned char teil4 = testInt.bytes[3];
+
+	printf("Int-Wert im Ganzen:   %d\n", testInt.anInt);
+	printf("Int-Wert im Ganzen:   %X\n", testInt.anInt);
+	
+	printf("Int-Wert Zerlegt:     %d - %X\n", teil1, teil1);
+	printf("Int-Wert Zerlegt:     %d - %X\n", teil2, teil2);
+	printf("Int-Wert Zerlegt:     %d - %X\n", teil3, teil3);
+	printf("Int-Wert Zerlegt:     %d - %X\n", teil4, teil4);
+}
+
+
 void strukturen()
 {
-	strukturen_uebung();
+	testUnion();
 }
